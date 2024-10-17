8bad672cc17b76e419b63fc9a4b0fca8f9724537
castrodev37 <devcastro37@gmail.com>
2024-10-17 20:33:06 -0300

    first commit

---
+++ s3.dpr
@@ -0,0 +1,14 @@
+program s3;
+
+uses
+  Vcl.Forms,
+  index in 'index.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
